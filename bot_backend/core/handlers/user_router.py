ed319f634a7e2409da29b0187ffa0cba3a7f619f
Haro Mikhaylov <www.haris@bk.ru>
2024-09-17 16:54:30 +0300

    add /help command handler

--- bot_backend/core/handlers/user_router.py
+++ bot_backend/core/handlers/user_router.py
@@ -1,11 +1,14 @@
+from asyncio import sleep
+
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
 from core.database.background_tasks import record_message_id_to_db
 from core.keyboards.start_keyboard import get_start_keyboard
-from core.middleware.settings import BOT
+from core.middleware.settings import BOT, DEL_TIME
 from core.middleware.wrappers import check_bd_chat_id, sub_check
+from core.utils.lang_greet import get_lang_greet_text
 
 user_router = Router()
 
@@ -26,13 +29,42 @@ async def cmd_start(message: Message):
 
     bot_info = await BOT.get_me()
     bot_name = bot_info.username
-    start_keyboard = await get_start_keyboard(message)
+    start_keyboard = get_start_keyboard(message)
 
     sent_message = await message.answer(
         text=f'<b>Здравствуйте, '
              f'<u>{message.from_user.first_name}</u>! \U0001F642'
              f'\nМеня зовут {bot_name}.</b>'
              f'\nЧем я могу вам помочь?',
+        reply_markup=start_keyboard.as_markup()
+    )
+
+    await record_message_id_to_db(sent_message)
+
+
+@user_router.message(Command('help'))
+@check_bd_chat_id
+async def cmd_help(message: Message, keep_last_msg: bool = False):
+    """
+    Handles the '/help' command when a user calls the main menu.
+
+    :param keep_last_msg: A Boolean value that determines whether the previous
+        message will be deleted
+    :param message:
+    :return:
+    """
+
+    await record_message_id_to_db(message)
+
+    if not keep_last_msg:
+        await BOT.delete_message(message.chat.id, message.message_id)
+
+    await sleep(DEL_TIME)
+
+    start_keyboard = get_start_keyboard(message)
+
+    sent_message = await message.answer(
+        get_lang_greet_text(message.from_user.first_name),
         reply_markup=start_keyboard.as_markup()
     )
 
